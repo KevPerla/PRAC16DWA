dab7b48a998fc3ca96263021eebb8d99b83ab0ca
josperla <78237563+josperla@users.noreply.github.com>
2023-12-09 19:01:45 -0600

    Practica 17

--- js/main.js
+++ js/main.js
@@ -2,37 +2,39 @@ tinymce.init({
     selector: '#contenido'
 });
 
-const btnAbrirForo = document.getElementById('btnAbrirForo')
-const btnCerrarForo = document.getElementById('btnCerrarForo')
-const btnPublicar = document.getElementById('btnPublicar')
-const formularioForo = document.getElementById('formularioForo')
-const publicaciones = document.getElementById('publicaciones')
-const contenido = document.getElementById('contenido')
-const classButtons = 'btn btn-'
+$(document).ready(function(){
+    $('#btnCerrarForo').hide();
+    $('#formularioForo').hide();
 
-btnAbrirForo.addEventListener('click', function() {
-    btnAbrirForo.setAttribute('class', 'd-none')
-    btnCerrarForo.removeAttribute('class')
-    btnCerrarForo.setAttribute('class', classButtons+ 'danger')
+    $('#btnAbrirForo').click(function () {
+        $('#btnCerrarForo').shoW();
+        $('#btnAbrirForo').hide();
+        $('#formularioForo').fadeIn('slow');
+    })
 
-    formularioForo.removeAttribute('class')
-    formularioForo.setAttribute('class', 'row')
-})
+    $('btnCerrarForo').click(function () {
+        $('btnAbrirForo').shoW();
+        $('btnCerrarForo').hide();
+        $('#formularioForo').fadeIn('slow');
+    })
 
-btnCerrarForo.addEventListener('click', function() {
-    btnCerrarForo.setAttribute('class', 'd-none')
-    btnAbrirForo.removeAttribute('class')
-    btnAbrirForo.setAttribute('class', classButtons +'primary')
 
-    formularioForo.removeAttribute('class')
-    formularioForo.setAttribute('class', 'd-none')
+    $('btnPublicar').click(function () {
+        $('btnAbrirForo').shoW();
+        $('btnCerrarForo').hide();
+        $('#formularioForo').fadeIn('slow');
+    })
 
-})
 
-btnPublicar.addEventListener('click', function() {
-    publicaciones.innerHTML = `
-    <div class="border p-2 mb-3 rounded-pill">
-    ${contenido}
-    </div>
-    `
+    const publicaciones = $('#publicaciones').html();
+
+    $('#publicaciones').html(
+        `${publicaciones}
+        <div class="border p-2 mb-3 rounded-2">
+        ${tinymce.get('contenido').getContent()}
+        </div>
+        `
+    ).fadeIn('slow');
+
+    tinymce.activeEditor.setContent('');
 })
